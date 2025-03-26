bddddb1a39bea5cc210ed327d448f6f362f76996
Mikal01 <mikal.boe@scaleaq.com>
2025-03-26 14:50:52 +0100

    ijgigf

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
-🧨 Remove this line!!! I'm making everything crash! 🧨
-
 import { createRoot } from "react-dom/client";
 import { App } from "./app";
+
 
 createRoot(document.getElementById("root")!).render(<App />);
